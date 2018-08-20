e9cdda4fe120a1a7fc8a0c7abeb8b52eeb7511aa
Rootjang92 <janggeunho7@gmail.com>
2018-08-20 18:43:10 +0900

    local value change

--- src/app/yapen-one/condition/condition.component.ts
+++ src/app/yapen-one/condition/condition.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { StateviewService } from '../stateview.service';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Rx';
 import { map, filter, scan, tap } from 'rxjs/operators';
 import { forEach } from '@angular/router/src/utils/collection';
 
@@ -14,8 +14,8 @@ import { forEach } from '@angular/router/src/utils/collection';
     <li style="width:260px" class="first-li" (click)="toggle('local')">
       <img src="../assets/glyphicons_free/glyphicons/png/glyphicons-39-plane.png" alt="지역" class="first-img">
       <div id="schLocationText">{{ res }}</div> <!-- 클릭하면 데이터가 바뀌도록  -->
-      <input type="hidden" name="schLoaction" id="schLoaction" value="인천/경기/가평">
-      <input type="hidden" name="schLoactionCode" id="schLoactionCode" value="1.001000">
+      <input type="hidden" name="schLoaction" id="schLoaction" [value]="res">
+      <input type="hidden" name="schLoactionCode" id="schLoactionCode" [value]="resid">
       <img src="../assets/glyphicons_free/glyphicons/png/glyphicons-602-chevron-down.png" alt="펼치기/닫기" class="arrowToggle last-img">
     </li>
     <li style="width:250px" (click)="toggle()">
@@ -45,14 +45,20 @@ import { forEach } from '@angular/router/src/utils/collection';
   `,
   styleUrls: [`./condition.css`]
 })
-export class ConditionComponent {
-  @Input() res: string;
+export class ConditionComponent implements OnInit {
+  @Input() res: any[];
+  @Input() resid: number;
   @Input() people = '전체';
+  location = [];
 
   url = 'https://www.pmb.kr/location/location-name';
 
   constructor(public stateviewService: StateviewService,
               private http: HttpClient) { }
+
+  ngOnInit() {
+    // this.getLocation();
+  }
 
   toggle(state: string) {
     if (this.stateviewService.state === state) {
@@ -60,5 +66,24 @@ export class ConditionComponent {
     } else {
       this.stateviewService.state = state;
     }
+  }
+
+  searchRoom() {
+    const basedUrl = 'https://www.pmb.kr/search/button_search/button_search/?';
+    this.http.get<any[]>(basedUrl);
+  }
+
+  getLocation() {
+    this.http.get<any[]>(this.url)
+      .subscribe( res => {
+        // console.log(res);
+        this.res = res;
+          res.filter((pension, i) => {
+            if ( pension.name === '가평' || pension.name === '경기') {
+                this.location.push(pension.sublocations);
+                // console.log(this.location);
+          }
+      });
+    });
   }
 }
